8a1b5dd9908316c3d002b14e446166124f51d3ba
Abhay Ajith <abhaytajith@gmail.com>
2024-02-20 22:20:52 +0530

    "First Commit"

--- app/src/main/java/com/example/musicplayer/ActionPlaying.java
+++ app/src/main/java/com/example/musicplayer/ActionPlaying.java
@@ -1,8 +1,12 @@
 package com.example.musicplayer;
 
+import android.media.MediaPlayer;
+
 public interface ActionPlaying {
 
     void playPauseBtnClicked();
     void prevBtnClicked();
     void nextBtnClicked();
+
+    void onCompletion(MediaPlayer mp);
 }
